842e74c401dd281520a94bfab499ed72705a3bd0
Oliver Zhang <ozhang@apple.com>
2018-04-12 17:05:30 -0700

    some more optimizations

--- zero_one_sequences/zero_one_sequences.py
+++ zero_one_sequences/zero_one_sequences.py
@@ -8,8 +8,8 @@ def main():
     branches = 1
     for char in stdin.readline():
         if char == '?': 
-            total       = ( (total*2) % M + ones) % M
-            ones        = ( (ones*2) % M + branches) % M
+            total       = (total*2 + ones) % M
+            ones        = (ones*2 + branches) % M
             branches    = (branches*2) % M
         elif char == '0':
             total = ( total + ones   ) % M
